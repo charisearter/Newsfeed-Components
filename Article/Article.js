7ad93cab8a05398c6163394446bd92a16cbdcd92
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-05-06 14:40:56 -0500

    elements set

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,18 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+function makeArticles(articleAttrs) {
+  const { title, date, firstParagraph, secondParagraph, thirdParagraph } = articleAttrs
+
+  const article = document.createElement('div')
+  const title = document.createElement('h2')
+  const date = document.createElement('p')
+  const firstParagraph = document.createElement('p')
+  const secondParagraph = document.createElement('p')
+  const thirdParagraph = document.createElement('p')
+  const button = document.createElement('span')
+
+//return the article
+  return article
+
+}
